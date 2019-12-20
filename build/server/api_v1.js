e2ccf4c33bd0cf1ce73579ffd73517191b1ce8c1
codlab <codlabtech@gmail.com>
2019-12-20 15:43:06 +0100

    log for files

--- build/server/api_v1.js
+++ build/server/api_v1.js
@@ -39,7 +39,7 @@ class APIv1 {
         console.log(`create file action on ${url} for path ${path}`);
         this._router.get(url, (req, res) => {
             const { code, id } = req.params;
-            console.log(`file ->> ${{ code, id }}`);
+            console.log("file ->> ", { code, id });
             if (!Lock_1.default.instance.valid(id, code)) {
                 res.status(400).json({ error: "invalid session" });
                 return;
@@ -58,7 +58,7 @@ class APIv1 {
         console.log(`create file action on ${url} for action ${action}`);
         this._router.get(url, (req, res) => {
             const { code, id } = req.params;
-            console.log("action params ->>" + code + " " + id);
+            console.log("action params ->> ", { code, id });
             if (!Lock_1.default.instance.valid(id, code)) {
                 res.status(400).json({ error: "invalid session" });
                 return;
